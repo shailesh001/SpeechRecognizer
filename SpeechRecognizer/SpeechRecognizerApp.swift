393592fc101fd57cbbfbac18a469c67179f4a3b0
Shailesh Patel <shailesh001@mac.com>
2020-12-28 13:20:10 +0000

    Initial Commit

---
+++ SpeechRecognizer/SpeechRecognizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechRecognizerApp.swift
+//  SpeechRecognizer
+//
+//  Created by Shailesh Patel on 28/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SpeechRecognizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
