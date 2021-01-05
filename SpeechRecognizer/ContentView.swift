692b0411434ff328c07cd7af6a80cde8c9727e50
Shailesh Patel <shailesh001@mac.com>
2021-01-05 11:51:49 +0000

    Updated solution

--- SpeechRecognizer/ContentView.swift
+++ SpeechRecognizer/ContentView.swift
@@ -5,12 +5,74 @@
 //  Created by Shailesh Patel on 28/12/2020.
 //
 
+import Speech
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    /*
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello, world!").padding()
+    }
+*/
+    @State var recording: Bool = false
+    @State var speech: String = ""
+
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                if !speech.isEmpty {
+                    Text(speech)
+                        .font(.largeTitle)
+                        .lineLimit(nil)
+                } else {
+                    Text("Speech will go here...")
+                        .font(.largeTitle)
+                        .foregroundColor(.gray)
+                        .lineLimit(nil)
+                }
+
+                Spacer()
+
+                if recording {
+                    Button(action: stopRecording) {
+                        ButtonLabel("Stop Recording", background: .red)
+                    }
+                } else {
+                    Button(action: startRecording) {
+                        ButtonLabel("Start Recording", background: .blue)
+                    }
+                }
+            }.padding()
+            .navigationBarTitle(Text("SRDemo"), displayMode: .inline)
+        }
+    }
+    
+    private let recognizer: SpeechRecognizer
+    
+    init() {
+        guard let recognizer = SpeechRecognizer() else {
+            fatalError("Something went wrong...")
+        }
+        self.recognizer = recognizer
+    }
+    
+    private func startRecording() {
+        self.recording = true
+        self.speech = ""
+        
+        recognizer.startRecording { result in
+            if let text = result {
+                self.speech = text
+            } else {
+                self.stopRecording()
+            }
+        }
+    }
+    
+    private func stopRecording() {
+        self.recording = false
+        recognizer.stopRecording()
     }
 }
 
@@ -19,3 +81,22 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct ButtonLabel: View {
+    private let title: String
+    private let background: Color
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            Text(title).font(.title).bold().foregroundColor(.white)
+            Spacer()
+        }.padding().background(background).cornerRadius(10)
+    }
+    
+    init(_ title: String, background: Color) {
+        self.title = title
+        self.background = background
+    }
+}
+
